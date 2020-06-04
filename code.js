204915be7a3db415490e703240a71c25fb91c3c6
Wu94 <wu_jeremy94@outlook.com>
2020-06-04 15:40:26 +1000

    firstcommit

---
+++ code.js
@@ -0,0 +1,16 @@
+const fetchPokemon = () => {
+    const promises = [];
+    for (let i = 1; i <= 150; i++) {
+        const url = `https://pokeapi.co/api/v2/pokemon/${i}`;
+        promises.push(fetch(url).then((res) => res.json()));
+    }
+    Promise.all(promises).then((results) => {
+        const pokemon = results.map((result) => ({
+            name: result.name,
+            image: result.sprites['front_default'],
+            type: result.types.map((type) => type.type.name).join(', '),
+            id: result.id
+        }));
+    });
+};
+
